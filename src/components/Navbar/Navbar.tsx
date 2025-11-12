833c38e29aef8c05fcac96e5da2ae434785ed10e
Matias Arias <matiasarias3103@gmail.com>
2025-11-12 13:39:34 -0300

    Base del navbar. Falta mejorar el estilo del navbar, posicion de los elementos y size de la fuente

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,9 +1,32 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import logo from "../../assets/logo.png";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons/faInstagram";
+import { faYoutube } from "@fortawesome/free-brands-svg-icons/faYoutube";
+import { faSquareFacebook } from "@fortawesome/free-brands-svg-icons/faSquareFacebook";
+import { faCircleUser } from "@fortawesome/free-regular-svg-icons/faCircleUser";
+import "../Navbar/Navbar.css";
+
 export default function Navbar() {
   return (
-    <div>
+    <header>
+      <h1>
+        <img src={logo} alt="Municipalidad de santiago" />
+      </h1>
       <ul>
-        <li>Hola esto es la navbar</li>
+        <li>Inicio</li>
+        {/* TODO: aqui se debe crear un componente de dropdown para agregar bien la funcionalidad */}
+        <li>Servicios</li>
+        <li>Agenda</li>
+        <li>Talleres</li>
+        {/* TODO: aqui se debe crear un componente de dropdown para agregar bien la funcionalidad */}
+        <li>Conocenos</li>
       </ul>
-    </div>
+      <div>
+        <FontAwesomeIcon icon={faInstagram} />
+        <FontAwesomeIcon icon={faYoutube} />
+        <FontAwesomeIcon icon={faSquareFacebook} />
+        <FontAwesomeIcon icon={faCircleUser} />
+      </div>
+    </header>
   );
 }
