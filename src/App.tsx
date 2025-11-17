3a262747b45f2dd52436aa8c6883d2fb16c14d3a
Matias Arias <matiasarias3103@gmail.com>
2025-11-17 02:18:36 -0300

    Arreglo de los errores de funcionalidad, no cargaba nada, no funcionaba nada

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import Home from "./components/pages/Home/Home";
 import PersonasMayores from "./components/pages/PersonasMayores/PersonasMayores";
 import { Base } from "./components/Base/Base";
 import Footer from "./components/Footer/Footer";
+import ServiciosSociales from "./components/pages/ServiciosSociales/ServiciosSociales";
+import GestionYAdministracionComunitaria from "./components/pages/GestionYAdministracionComunitaria/GestionYAdministracionComunitaria";
+import DireccionDeDesarrolloComunitario from "./components/pages/DirecciónDeDesarrolloComunitario/DireccionDeDesarrolloComunitario";
 
 function App() {
   return (
@@ -15,6 +18,19 @@ function App() {
         <Route
           path="/personas-mayores"
           element={<Base content={<PersonasMayores />} />}
+        />
+        <Route
+          path="/servicios-sociales"
+          element={<Base content={<ServiciosSociales />} />}
+        />
+        <Route
+          path="/gestion-administracion-comunitaria"
+          element={<Base content={<GestionYAdministracionComunitaria />} />}
+        />
+
+        <Route
+          path="/direccion-desarrollo-comunitario"
+          element={<Base content={<DireccionDeDesarrolloComunitario />} />}
         />
       </Routes>
       <Footer />
