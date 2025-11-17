9159a883873fe051f20e5f8fd39af6cec33b75bb
Matias Arias <matiasarias3103@gmail.com>
2025-11-17 04:05:27 -0300

    Agregado de las paginas personas mayores, migrantes, participacion, pueblos originarios

---
+++ src/components/pages/PueblosOriginarios/PueblosOriginarios.tsx
@@ -0,0 +1,22 @@
+import { Card } from "../ParticipacionCiudadana/Card";
+import indigenas from "../../../assets/pueblosOriginarios/indigenas.png";
+
+const informacionJSON = [
+  {
+    id: 1,
+    img: indigenas,
+    titulo: "Pueblos Indígenas",
+  },
+];
+export default function PueblosOriginarios() {
+  return (
+    <>
+      <h2>Pueblos Originarios</h2>
+      <div className="tarjetas-contenedor">
+        {informacionJSON.map((element) => (
+          <Card key={element.id} item={element} />
+        ))}
+      </div>
+    </>
+  );
+}
