41e653c81efd10ea56fc7f4c33f637b083aafd3f
Matias Arias <matiasarias3103@gmail.com>
2025-11-17 04:21:36 -0300

    Arreglo del css

--- src/components/pages/Migrantes/Migrantes.tsx
+++ src/components/pages/Migrantes/Migrantes.tsx
@@ -1,6 +1,7 @@
 import vinculacion from "../../../assets/migrantes/vinculacion-medio.png";
 import atencion from "../../../assets/migrantes/atencion-social.png";
 import { Card } from "../ParticipacionCiudadana/Card";
+import "./Migrantes.css";
 const informacionJSON = [
   {
     id: 1,
