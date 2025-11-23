eebf94a55afc19fa543c5b6a4351f61349169064
Matias Arias <matiasarias3103@gmail.com>
2025-11-23 15:06:30 -0300

    Cambios

--- src/components/pages/DiversidadSexual/DiversidadSexual.tsx
+++ src/components/pages/DiversidadSexual/DiversidadSexual.tsx
@@ -1,32 +1,33 @@
 import "./DiversidadSexual.css";
-import wave from "../../../assets/wave-effect.jpg";
 import ApoyoLaboralT from "../../../assets/DiversidadSexualImg/ApoyoLaboralTrans.jpg";
 import ProgramaSocialA from "../../../assets/DiversidadSexualImg/ProgramaSocialAcompanamiento.jpg";
 import CentroAtencionF from "../../../assets/DiversidadSexualImg/CentroAtencionFamilia.jpg";
 
+const cardInfo = [
+  {
+    id: 1,
+    img: ApoyoLaboralT,
+    titulo: "Apoyo Laboral Trans",
+  },
+  {
+    id: 2,
+    img: ProgramaSocialA,
+    titulo: "Programa social de acompañamiento",
+  },
+  {
+    id: 3,
+    img: CentroAtencionF,
+    titulo:
+      "Programa de atención a personas LGTBIQ+ víctimas de violencia en contexto de pareja",
+  },
+];
+
 export default function DiversidadSexual() {
   return (
-    <div>
+    <>
       <h2 className="titulo-pagina">Diversidad Sexual</h2>
       <div className="grid-tarjetas">
-        {[
-          {
-            id: 1,
-            img: ApoyoLaboralT,
-            titulo: "Apoyo Laboral Trans",
-          },
-          {
-            id: 2,
-            img: ProgramaSocialA,
-            titulo: "Programa social de acompañamiento",
-          },
-          {
-            id: 3,
-            img: CentroAtencionF,
-            titulo:
-              "Programa de atención a personas LGTBIQ+ víctimas de violencia en contexto de pareja",
-          },
-        ].map((item) => (
+        {cardInfo.map((item) => (
           <div className="tarjeta" key={item.id}>
             <img src={item.img} alt={item.titulo} className="imagen-tarjeta" />
             <h3 className="titulo-tarjeta">{item.titulo}</h3>
@@ -34,8 +35,6 @@ export default function DiversidadSexual() {
           </div>
         ))}
       </div>
-      {/* Imagen inferior olas */}
-      <img src={wave} className="wave-img" />
-    </div>
+    </>
   );
 }
